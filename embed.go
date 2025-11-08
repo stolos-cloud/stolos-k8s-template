928bd89dd47ab3bfb98bd928d4e6d98da7d9a96c
Simon-Boyer <si.boyer@hotmail.ca>
2025-11-08 16:08:49 -0500

    yoke ci/cd and new repo format

--- embed.go
+++
@@ -1,6 +0,0 @@
-package k8s_manifests
-
-import _ "embed"
-
-//go:embed argocd/helm/values.yaml
-var ArgoValuesYaml []byte
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/stolos-cloud/stolos/k8s_manifests
-
-go 1.25
